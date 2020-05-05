8d8e37508275382845518c94307a70c9e5c42e5a
Bhrati <GBHRATI@volvocars.com>
2020-05-05 18:01:31 +0530

    adding admin module

---
+++ src/app/admin/admin/update-account/update-account/update-account.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UpdateAccountComponent } from './update-account.component';
+
+
+
+@NgModule({
+  declarations: [UpdateAccountComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UpdateAccountModule { }
