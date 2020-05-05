c9006581d1fee3d29c32c8c3df9f437ff7cfd155
Bhrati <GBHRATI@volvocars.com>
2020-05-05 19:09:37 +0530

    adding customer module

---
+++ src/app/customer/detail-statemnt/detail-statemnt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-statemnt',
+  templateUrl: './detail-statemnt.component.html',
+  styleUrls: ['./detail-statemnt.component.css']
+})
+export class DetailStatemntComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
