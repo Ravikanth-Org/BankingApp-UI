82a3a76323ae52f730f316470f5d71f13540c5f5
Bhrati <GBHRATI@volvocars.com>
2020-05-09 20:19:56 +0530

    Added personal details with sample data

--- src/app/customer/customer-home/customer-home.component.ts
+++ src/app/customer/customer-home/customer-home.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerHomeComponent implements OnInit {
 
+userDetails={
+address:"xyz bangalore",
+city:"Bangalore",
+dob:new Date(),
+name:"Ram Kumar",
+phone:8787287290,
+pin:603103,
+email:'ram@gmail.com',
+userId:"34ser7",
+accountType:"saving"
+
+
+}
+
   constructor() { }
 
   ngOnInit(): void {
