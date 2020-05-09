cea59c84f6413828f081a3d51d75b2be32105072
Bhrati <GBHRATI@volvocars.com>
2020-05-09 16:47:13 +0530

    details statment with hardcoaded data

--- src/app/customer/detail-statemnt/detail-statemnt.component.ts
+++ src/app/customer/detail-statemnt/detail-statemnt.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-detail-statemnt',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-statemnt.component.css']
 })
 export class DetailStatemntComponent implements OnInit {
+  detailSatementForm: FormGroup;
+  showTable=false;
+  userTranscationDetails=[
+    {
+      transactionId: 2653635,
+      transactiontime: new Date(),
+      transtype: 'atm',
+      credit: 82783,
+      balance: 989754,
+      chequeNumber:"2736DFg"
+    },
+    {
+      transactionId: 7363736,
+      transactiontime: new Date(),
+      transtype: 'card',
+      credit: 5363,
+      balance: 149754,
+      chequeNumber:"6256Tty"
+    },
+    {
+      transactionId: 4543,
+      transactiontime: new Date(),
+      transtype: 'atm',
+      credit: 7265,
+      balance: 1469897,
+      chequeNumber:"6256TYu"
+    },
+  ]
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.detailSatementForm = this.formBuilder.group({
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+
+
+    })
+
+  }
+  onSubmit(selectedDates: FormGroup) {
+  this.showTable=true;
+
+    console.log("on submit click")
+
   }
 
 }
